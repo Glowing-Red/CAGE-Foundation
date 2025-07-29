719c5d4aac1e11b96b98b6c924c5dd53100c6c5f
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2025-07-29 13:17:36 +0200

    f

--- Assets/Prefabs/Anomaly/Setup.js
+++ Assets/Prefabs/Anomaly/Setup.js
@@ -4,67 +4,67 @@ const titleFilter = ["Tags"];
 let content;
 
 async function Init() {
-   try {
-      const anomalyTemplate = await FetchTemplate();
-      
-      const parser = new DOMParser();
-      const anomalyDoc = parser.parseFromString(anomalyTemplate, "text/html");
-      
-      await LoadDocument(anomalyDoc.head, document.head);
-      await LoadDocument(anomalyDoc.body, document.body);
-      
-      const headerPrefab = await FetchPrefab("Header");
-      const anomalyPrefab = await FetchPrefab("Anomaly");
+    try {
+        const anomalyTemplate = await FetchTemplate();
 
-      const headerPrefabDoc = parser.parseFromString(headerPrefab, "text/html");
-      const anomalyPrefabDoc = parser.parseFromString(anomalyPrefab, "text/html");
-      
-      const headerClone = headerPrefabDoc.querySelector("template").content.cloneNode(true).querySelector('.header');
-      const anomalyClone = anomalyPrefabDoc.querySelector("template").content.cloneNode(true).querySelector('.container');
-      
-      document.body.appendChild(headerClone);
-      document.body.appendChild(anomalyClone);
-      
-      const table = await GetTable();
-      
-      content = anomalyClone.querySelector(".content");
-      document.title = `Cage: Anomaly-${table["Index"]}`
-      document.documentElement.style.setProperty("--header-height", `${headerClone.getBoundingClientRect().height}px`);
-      
-      const docDiv = Instance("div", {}, content);
-      if (isValidString(table["Title"])) {
-         Instance("h2", {
-            "Text": table["Title"]
-         }, docDiv);
-      } else {
-         Instance("h2", {
-            "Text": `Anomaly-${table["Index"]}`
-         }, docDiv);
-      }
-      
-      Instance("p", {
-         "Html": await FormatText(`**Title:** ${isValidString(table["Title"]) ? table["Title"] : "[MISSING]"}`)
-      }, docDiv);
-      Instance("p", {
-         "Html": await FormatText(`**Index:** #${table["Index"]}`)
-      }, docDiv);
+        const parser = new DOMParser();
+        const anomalyDoc = parser.parseFromString(anomalyTemplate, "text/html");
 
-      for (const [key, value] of Object.entries(table)) {
-         if (IsTable(value)) {
-            if (!titleFilter.includes(key)) {
-               await CreateTitle(value, key);
+        await LoadDocument(anomalyDoc.head, document.head);
+        await LoadDocument(anomalyDoc.body, document.body);
+
+        const headerPrefab = await FetchPrefab("Header");
+        const anomalyPrefab = await FetchPrefab("Anomaly");
+
+        const headerPrefabDoc = parser.parseFromString(headerPrefab, "text/html");
+        const anomalyPrefabDoc = parser.parseFromString(anomalyPrefab, "text/html");
+
+        const headerClone = headerPrefabDoc.querySelector("template").content.cloneNode(true).querySelector('.header');
+        const anomalyClone = anomalyPrefabDoc.querySelector("template").content.cloneNode(true).querySelector('.container');
+
+        document.body.appendChild(headerClone);
+        document.body.appendChild(anomalyClone);
+
+        const table = await GetTable();
+
+        content = anomalyClone.querySelector(".content");
+        document.title = `Cage: Anomaly-${table["Index"]}`
+        document.documentElement.style.setProperty("--header-height", `${headerClone.getBoundingClientRect().height}px`);
+
+        const docDiv = Instance("div", {}, content);
+        if (isValidString(table["Title"])) {
+            Instance("h2", {
+                "Text": table["Title"]
+            }, docDiv);
+        } else {
+            Instance("h2", {
+                "Text": `Anomaly-${table["Index"]}`
+            }, docDiv);
+        }
+
+        Instance("p", {
+            "Html": await FormatText(`**Title:** ${isValidString(table["Title"]) ? table["Title"] : "[MISSING]"}`)
+        }, docDiv);
+        Instance("p", {
+            "Html": await FormatText(`**Index:** #${table["Index"]}`)
+        }, docDiv);
+
+        for (const [key, value] of Object.entries(table)) {
+            if (IsTable(value)) {
+                if (!titleFilter.includes(key)) {
+                    await CreateTitle(value, key);
+                }
+            } else {
+                if (!paragraphFilter.includes(key)) {
+                    await CreateParagraph(key, value);
+                }
             }
-         } else {
-            if (!paragraphFilter.includes(key)) {
-               await CreateParagraph(key, value);
-            }
-         }
-      }
-      
-      CreateParagraph("Conclusion", table["Conclusion"]);
-   } catch (error) {
-      console.error("Initialization error:", error);
-   }
+        }
+
+        CreateParagraph("Conclusion", table["Conclusion"]);
+    } catch (error) {
+        console.error("Initialization error:", error);
+    }
 };
 
 async function CreateTag(table) {
@@ -72,145 +72,145 @@ async function CreateTag(table) {
 }
 
 async function CreateParagraph(title, text) {
-   const div = Instance("div", {});
-   
-   Instance("p", {
-      "Html": await FormatText(FormatString("**%s:** %s", title, text))
-   }, div);
-   
-   div.Parent = content;
-   
-   return div;
+    const div = Instance("div", {});
+
+    Instance("p", {
+        "Html": await FormatText(FormatString("**%s:** %s", title, text))
+    }, div);
+
+    div.Parent = content;
+
+    return div;
 }
 
 async function CreateTitle(table, item) {
-   const length = GetLength(table);
+    const length = GetLength(table);
 
-   if (length <= 1) {
-      return;
-   }
-   
-   const titleDiv = Instance("div", {});
-   const itemsContainer = Instance("div", {
-      "Class": "flex-column",
-      "Style": {
-         "gap": "15px"
-      }
-   });
-   
-   Instance("h2", {
-      "Text": length > 2 ? item + "s" : item
-   }, titleDiv);
+    if (length <= 1) {
+        return;
+    }
 
-   for (const [key, value] of Object.entries(table)) {
-      if (IsTable(value)) {
-         const itemDiv = Instance("div", {
-            "Style": {
-               "marginLeft": "20px"
-            }
-         });
-         const itemContainer = Instance("div", {
-            "Class": "flex-column",
-            "Style": {
-               "marginLeft": "20px",
-               "gap": "5px"
-            }
-         });
-         
-         Instance("h3", {
-            "Text": FormatString(table["Title"], key, value["Title"])
-         }, itemDiv);
-         
-         for (const [key_2, value_2] of Object.entries(value)) {
-            if (key_2 !== "Title") {
-               if (IsTable(value_2)) {
-                  const keyDiv = Instance("div", {}, itemContainer);
-                  const container = Instance("div", {
-                     "Style": {
-                        "marginLeft": "15px"
-                     }
-                  });
-                  
-                  Instance("p", {
-                     "Html": await FormatText(`**${key_2}:**`)
-                  }, keyDiv);
-                  
-                  for (const [key_3, value_3] of Object.entries(value_2)) {
-                     Instance("p", {
-                        "Html": await FormatText(`**${key_3}:** ${value_3}`)
-                     }, container);
-                  }
-                  
-                  container.Parent = keyDiv
-               } else {
-                  Instance("p", {
-                     "Html": await FormatText(`**${key_2}:** ${value_2}`)
-                  }, itemContainer);
-               }
-            }
-         }
-         
-         itemContainer.Parent = itemDiv;
-         itemDiv.Parent = itemsContainer;
-      }
-   }
-   
-   itemsContainer.Parent = titleDiv;
-   titleDiv.Parent = content;
+    const titleDiv = Instance("div", {});
+    const itemsContainer = Instance("div", {
+        "Class": "flex-column",
+        "Style": {
+            "gap": "15px"
+        }
+    });
 
-   return titleDiv;
+    Instance("h2", {
+        "Text": length > 2 ? item + "s" : item
+    }, titleDiv);
+
+    for (const [key, value] of Object.entries(table)) {
+        if (IsTable(value)) {
+            const itemDiv = Instance("div", {
+                "Style": {
+                    "marginLeft": "20px"
+                }
+            });
+            const itemContainer = Instance("div", {
+                "Class": "flex-column",
+                "Style": {
+                    "marginLeft": "20px",
+                    "gap": "5px"
+                }
+            });
+
+            Instance("h3", {
+                "Text": FormatString(table["Title"], key, value["Title"])
+            }, itemDiv);
+
+            for (const [key_2, value_2] of Object.entries(value)) {
+                if (key_2 !== "Title") {
+                    if (IsTable(value_2)) {
+                        const keyDiv = Instance("div", {}, itemContainer);
+                        const container = Instance("div", {
+                            "Style": {
+                                "marginLeft": "15px"
+                            }
+                        });
+
+                        Instance("p", {
+                            "Html": await FormatText(`**${key_2}:**`)
+                        }, keyDiv);
+
+                        for (const [key_3, value_3] of Object.entries(value_2)) {
+                            Instance("p", {
+                                "Html": await FormatText(`**${key_3}:** ${value_3}`)
+                            }, container);
+                        }
+
+                        container.Parent = keyDiv
+                    } else {
+                        Instance("p", {
+                            "Html": await FormatText(`**${key_2}:** ${value_2}`)
+                        }, itemContainer);
+                    }
+                }
+            }
+
+            itemContainer.Parent = itemDiv;
+            itemDiv.Parent = itemsContainer;
+        }
+    }
+
+    itemsContainer.Parent = titleDiv;
+    titleDiv.Parent = content;
+
+    return titleDiv;
 }
 
 function LoadDocument(sourceDoc, targetDoc) {
-   return new Promise((resolve, reject) => {
-      targetDoc.innerHTML = sourceDoc.innerHTML;
+    return new Promise((resolve, reject) => {
+        targetDoc.innerHTML = sourceDoc.innerHTML;
 
-      const scripts = sourceDoc.querySelectorAll('script');
-      let scriptLoadPromises = [];
-      
-      scripts.forEach(oldScript => {
-         const newScript = document.createElement('script');
-         newScript.type = oldScript.type ? oldScript.type : 'text/javascript';
-   
-         if (oldScript.src) {
-            newScript.src = oldScript.src;
-            newScript.async = false;
-            
-            let scriptPromise = new Promise((resolve, reject) => {
-            newScript.onload = resolve;
-            newScript.onerror = reject;
-            })
+        const scripts = sourceDoc.querySelectorAll('script');
+        let scriptLoadPromises = [];
 
-            scriptLoadPromises.push(scriptPromise);
-            targetDoc.appendChild(newScript);
-         } else {
-            newScript.textContent = oldScript.innerText;
-            targetDoc.appendChild(newScript);
-         }
-      });
-      
-      Promise.all(scriptLoadPromises).then(resolve).catch(reject);
-   });
+        scripts.forEach(oldScript => {
+            const newScript = document.createElement('script');
+            newScript.type = oldScript.type ? oldScript.type : 'text/javascript';
+
+            if (oldScript.src) {
+                newScript.src = oldScript.src;
+                newScript.async = false;
+
+                let scriptPromise = new Promise((resolve, reject) => {
+                    newScript.onload = resolve;
+                    newScript.onerror = reject;
+                })
+
+                scriptLoadPromises.push(scriptPromise);
+                targetDoc.appendChild(newScript);
+            } else {
+                newScript.textContent = oldScript.innerText;
+                targetDoc.appendChild(newScript);
+            }
+        });
+
+        Promise.all(scriptLoadPromises).then(resolve).catch(reject);
+    });
 }
 
 function FetchTemplate() {
-   return fetch("../../Prefabs/Anomaly/Template.html").then(response => {
-      if (!response.ok) {
-         throw new Error("Network response was not ok " + response.statusText);
-      }
-      
-      return response.text();
-   });
+    return fetch("../../Prefabs/Anomaly/Template.html").then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok " + response.statusText);
+        }
+
+        return response.text();
+    });
 }
 
 function GetTable() {
-   return fetch("./File.json").then(response => {
-      if (!response.ok) {
-         throw new Error("Network response was not ok " + response.statusText);
-      }
-      
-      return response.json();
-   });
+    return fetch("./File.json").then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok " + response.statusText);
+        }
+
+        return response.json();
+    });
 }
 
 // Initialize the script
--- Assets/Scripts/Utilities.js
+++ Assets/Scripts/Utilities.js
@@ -1,145 +1,145 @@
 function FetchJson(jsonPath) {
-   return fetch(jsonPath).then(response => {
-      if (!response.ok) {
-         throw new Error("Network response was not ok " + response.statusText);
-      }
-      
-      return response.json();
-   });
+    return fetch(jsonPath).then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok " + response.statusText);
+        }
+
+        return response.json();
+    });
 }
 
 function FetchPrefab(targetPrefab) {
-   return fetch(`../../Prefabs/${targetPrefab}.html`).then(response => {
-      if (!response.ok) {
-         throw new Error("Network response was not ok " + response.statusText);
-      }
-      
-      return response.text();
-   });
+    return fetch(`../../Prefabs/${targetPrefab}.html`).then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok " + response.statusText);
+        }
+
+        return response.text();
+    });
 }
 
 function FormatString(template, ...values) {
-   return template.replace(/%s/g, () => values.shift());
+    return template.replace(/%s/g, () => values.shift());
 }
 
 function FormatText(text) {
-   return new Promise((resolve, reject) => {
-      const regex = /\[(.*?)\]\((.*?)\)/g;
-      const promises = [];
-      
-      let formattedText = text
-         .replace(/\*\*\*(.*?)\*\*\*/g, `<span class="italic bold">$1</span>`)
-         .replace(/\*\*(.*?)\*\*/g, `<span class="bold">$1</span>`)
-         .replace(/\*(.*?)\*/g, `<span class="italic">$1</span>`)
-         .replace(/\^\^(.*?)\^\^/g, `<sup>$1</sup>`)
-         .replace(/,,(.*?),,/g, `<sub>$1</sub>`)
-         .replace(/__(.*?)__/g, `<u>$1</u>`)
-         .replace(/--(.*?)--/g, `<del>$1</del>`)
-         .replace(/\n(\d+)/g, (_, n) => {
-            const num = Number(n);
-            
-            return num > 1 ? '<br>'.repeat(num) : '';
-         })
-         .replace(/\n/g, `<br>`);
-      let match;
-      
-      while ((match = regex.exec(text)) !== null) {
-         const [fullMatch, linkText, href] = match;
-         
-         if (linkText === "Anomaly") {
-            const url = `../${href}`;
+    return new Promise((resolve, reject) => {
+        const regex = /\[(.*?)\]\((.*?)\)/g;
+        const promises = [];
 
-            const promise = FetchJson(`${url}/File.Json`).then(json => {
-               if (json) {
-                  if (json["Title"]) {
-                     formattedText = formattedText.replace(fullMatch, `<a href="${`${url}/Anomaly.html`}"><span>${json["Title"]}</span></a>`);
-                  } else {
-                     formattedText = formattedText.replace(fullMatch, `<a href="${`${url}/Anomaly.html`}"><span>${`Anomaly-${href}`}</span></a>`);
-                  }
-               } else {
-                  formattedText = formattedText.replace(fullMatch, `<a href=""><span>[REDACTED]</span></a>`);
-               }
-           }).catch(error => {
-               formattedText = formattedText.replace(fullMatch, `<a href=""><span>[REDACTED]</span></a>`);
-               console.error("Error fetching JSON:", error);
-           });
-           
-           promises.push(promise);
-         } else {
-            text = text.replace(fullMatch, `<a href="${href}"><span>${linkText}</span></a>`);
-         }
-      }
-      
-      Promise.all(promises).then(() => {
-         resolve(formattedText);
-     }).catch(reject);
-   });
+        let formattedText = text
+            .replace(/\*\*\*(.*?)\*\*\*/g, `<span class="italic bold">$1</span>`)
+            .replace(/\*\*(.*?)\*\*/g, `<span class="bold">$1</span>`)
+            .replace(/\*(.*?)\*/g, `<span class="italic">$1</span>`)
+            .replace(/\^\^(.*?)\^\^/g, `<sup>$1</sup>`)
+            .replace(/,,(.*?),,/g, `<sub>$1</sub>`)
+            .replace(/__(.*?)__/g, `<u>$1</u>`)
+            .replace(/--(.*?)--/g, `<del>$1</del>`)
+            .replace(/\n(\d+)/g, (_, n) => {
+                const num = Number(n);
+
+                return num > 1 ? '<br>'.repeat(num) : '';
+            })
+            .replace(/\n/g, `<br>`);
+        let match;
+
+        while ((match = regex.exec(text)) !== null) {
+            const [fullMatch, linkText, href] = match;
+
+            if (linkText === "Anomaly") {
+                const url = `../${href}`;
+
+                const promise = FetchJson(`${url}/Document.json`).then(json => {
+                    if (json) {
+                        if (json["Title"]) {
+                            formattedText = formattedText.replace(fullMatch, `<a href="${`${url}/Anomaly.html`}"><span>${json["Title"]}</span></a>`);
+                        } else {
+                            formattedText = formattedText.replace(fullMatch, `<a href="${`${url}/Anomaly.html`}"><span>${`Anomaly-${href}`}</span></a>`);
+                        }
+                    } else {
+                        formattedText = formattedText.replace(fullMatch, `<a href=""><span>[REDACTED]</span></a>`);
+                    }
+                }).catch(error => {
+                    formattedText = formattedText.replace(fullMatch, `<a href=""><span>[REDACTED]</span></a>`);
+                    console.error("Error fetching JSON:", error);
+                });
+
+                promises.push(promise);
+            } else {
+                text = text.replace(fullMatch, `<a href="${href}"><span>${linkText}</span></a>`);
+            }
+        }
+
+        Promise.all(promises).then(() => {
+            resolve(formattedText);
+        }).catch(reject);
+    });
 }
 
 function IsTable(item) {
-   return Object.prototype.toString.call(item) === "[object Object]";
+    return Object.prototype.toString.call(item) === "[object Object]";
 }
 
 function isValidString(value) {
-   return (typeof value === 'string' && value.trim() !== '' && value !== null);
+    return (typeof value === 'string' && value.trim() !== '' && value !== null);
 }
 
 function GetLength(table) {
-   return Object.entries(table).length;
+    return Object.entries(table).length;
 }
 
 function PropertyConvert(property) {
-   const Properties = {
-      "Html": "innerHTML",
-      "Text": "textContent",
-      "Class": "className",
-      "Style": "style"
-   }
-   
-   if (Properties[property] != null) {
-      return Properties[property];
-   }
-   
-   return null;
+    const Properties = {
+        "Html": "innerHTML",
+        "Text": "textContent",
+        "Class": "className",
+        "Style": "style"
+    }
+
+    if (Properties[property] != null) {
+        return Properties[property];
+    }
+
+    return null;
 }
 
 function Instance(Instance, Properties, Parent) {
-   const element = document.createElement(Instance);
-   
-   for (const [key, value] of Object.entries(Properties)) {
-      if (PropertyConvert(key)) {
-         if (IsTable(value)) {
-            for (const [key_2, value_2] of Object.entries(value)) {
-               element[PropertyConvert(key)][key_2] = value_2;
-            }
-         } else {
-            element[PropertyConvert(key)] = value;
-         }
-      }
-   }
-   
-   Object.defineProperty(element, "Parent", {
-      get() {
-         return this._parent;
-      },
-      set(newParent) {
-         if (this._parent) {
-            this._parent.removeChild(this);
-         }
+    const element = document.createElement(Instance);
 
-         if (newParent) {
-            newParent.appendChild(this);
-         }
-         
-         this._parent = newParent;
-      }
-   });
-   
-   if (Parent) {
-      element.Parent = Parent
-   }
-   
-   return element;
+    for (const [key, value] of Object.entries(Properties)) {
+        if (PropertyConvert(key)) {
+            if (IsTable(value)) {
+                for (const [key_2, value_2] of Object.entries(value)) {
+                    element[PropertyConvert(key)][key_2] = value_2;
+                }
+            } else {
+                element[PropertyConvert(key)] = value;
+            }
+        }
+    }
+
+    Object.defineProperty(element, "Parent", {
+        get() {
+            return this._parent;
+        },
+        set(newParent) {
+            if (this._parent) {
+                this._parent.removeChild(this);
+            }
+
+            if (newParent) {
+                newParent.appendChild(this);
+            }
+
+            this._parent = newParent;
+        }
+    });
+
+    if (Parent) {
+        element.Parent = Parent
+    }
+
+    return element;
 }
 
 /*
